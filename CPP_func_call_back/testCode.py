f5f9861299757ffc60fd0df36d5a373a29fbe139
hongfeng <hm@nilt.com>
2024-07-31 17:01:23 +0200

    add python test code

---
+++ CPP_func_call_back/testCode.py
@@ -0,0 +1,32 @@
+import numpy as np 
+import testModule 
+
+tt=testModule.TestModule()
+
+print(dir(tt))
+
+N=np.int64(10)
+x=np.linspace(0,1,N, dtype=np.float64)
+
+x2=np.linspace(0,1,20, dtype=np.float64)
+
+tt.setX(x)
+
+print(tt.n0)
+
+tt._print()
+
+#for i0 in range(tt.n0):
+#    print(tt.x0[0])
+
+def func(data):
+    for d in data:
+        print(d)
+
+tt.setFunc(func)
+
+print("set func done")
+print()
+
+tt.test(x2)
+
